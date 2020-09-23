584f2a0866bde011d79ee2ede52c8ed7813ef843
Chantal Snazel <clsnazel@gmail.com>
2020-09-23 20:39:58 +0000

    added movement and input. snake now moves on server

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const { connect } = require('./client');
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-  //stdin.on('data', data => handleUserInput(data));
-  stdin.on('data', handleUserInput);
-  //stdin.on('data', handleUserInput(data)); // wrong
-  // () => {}
-  return stdin;
-};
+const { setupInput } = require('./input');
 
 
 
-setupInput();
+let conn = connect();
+setupInput(conn);
 console.log('connecting....');
-connect();
